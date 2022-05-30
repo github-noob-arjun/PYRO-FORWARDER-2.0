bb64de7f9e514822ca7a82dd8a814da978e7ab98
Mr muha <91768693+MuhaKn@users.noreply.github.com>
2022-05-30 21:11:33 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,43 +1,37 @@
 import os
 from config import Config
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 import asyncio
 import sys
 
 START_MSG="Hi {},\nThis is a simple bot to forward all messages from one channel to other\n\n⚠️Warning\nYour account may get banned if you forward more files(from private channels). Use at Own Risk!!"
-HELP_MSG="Available commands:-\n\n/index - To index a channel\n/forward - To start forwarding\n/total - Count total messages in DB\n/status - Check Current status\n/help - Help data\n/stop - To stop all running processes. \n\nUse /index to index messages from a channel to database.\n\nAfter indexing you can start forwarding by using /forward.\n\n<b>Note:</b>\nYou will require the following data to index a channel:-\n\n<b>Channel Invite Link</b>:- If channel is a Private channel User needs to join channel to acces the messages. Please note that do not leave channel until forwarding completes.\n\n<b>Channel ID</b>:- If channel is a private channel you may need to enter Channel ID. Get it from @ChannelidHEXbot.\n\n<b>SKIP_NO</b>:-From where you want to start Forwarding files.Give 0 if from starting\n\n<b>Caption</b>:- Custom Caption for forwarded files. Use 0 to use default captions."
-buttons=InlineKeyboardMarkup(
-        [
-            [
-                InlineKeyboardButton("Help", callback_data="help"),
-                InlineKeyboardButton("How Does This Works?", callback_data="abt")
-            ],
-            [
-                InlineKeyboardButton("Source Code", url="https://github.com/subinps/Forward_2.0"),
-                InlineKeyboardButton("Report a Bug", url="https://t.me/subinps")
-            ]
-        ]
-        )
+
+HELP_MSG="Available commands:-\n\n/index - To index a channel\n/forward - To start forwarding\n/total - Count total messages in DB\n/status - Check Current status\n/help - Help data\n/stop - To stop all running processes. \n/cleardb - clear all files in database.\n\nUse /index to index messages from a channel to database.\n\nAfter indexing you can start forwarding by using /forward.\n\n<b>Note:</b>\nYou will require the following data to index a channel:-\n\n<b>Channel Invite Link</b>:- If channel is a Private channel User needs to join channel to acces the messages. Please note that do not leave channel until forwarding completes.\n\n<b>Channel ID</b>:- If channel is a private channel you may need to enter Channel ID. Get it from @ChannelidHEXbot.\n\n<b>SKIP_NO</b>:-From where you want to start Forwarding files.Give 0 if from starting\n\n<b>Caption</b>:- Custom Caption for forwarded files. Use 0 to use default captions."
+
+ABOUT_TXT="""
 
 @Client.on_message(filters.private & filters.command('start'))
 async def start(client, message):
-    await client.send_message(
-        chat_id=message.chat.id,
-        text=START_MSG.format(
-                message.from_user.first_name),
-        reply_markup=buttons,
-        parse_mode="html")
-
+    await message.reply_text(
+        text=START_MSG.format(message.from_user.first_name),
+        reply_markup=InlineKeyboardMarkup( [[
+            InlineKeyboardButton("ℹ️ HELP", callback_data="help"),
+            InlineKeyboardButton("💫 ABOUT", callback_data="abt")
+            ],[
+            InlineKeyboardButton("🍂 SUPPORT 🍂", url="https://t.me/PYRO_BOTZ_CHAT")
+            ]]
+            )
+        )
 
 @Client.on_message(filters.command("stop"))
 async def stop_button(bot, message):
 
     if str(message.from_user.id) not in Config.OWNER_ID:
         return
-    msg = await bot.send_message(
+    msg = await message.reply_text(
         text="Stoping all processes...",
-        chat_id=message.chat.id
+        #chat_id=message.chat.id
     )
     await asyncio.sleep(1)
     await msg.edit("All Processes Stopped and Restarted")
@@ -46,26 +40,49 @@ async def stop_button(bot, message):
 
 @Client.on_message(filters.private & filters.command('help'))
 async def help(client, message):
-    await client.send_message(
-        chat_id=message.chat.id,
+    await message.reply_text(
+        #chat_id=message.chat.id,
         text=HELP_MSG,
         parse_mode="html")
 
 
-@Client.on_callback_query(filters.regex(r'^help$'))
-async def cb_help(bot, cb):
-    await cb.message.edit_text(HELP_MSG)
+@Client.on_callback_query()
+async def cb_handler(client, query: CallbackQuery):
+    data = query.data 
+    if data == "start":
+        await query.message.edit_text(
+            text=START_MSG.format(query.from_user.first_name),
+            reply_markup=InlineKeyboardMarkup( [[
+                InlineKeyboardButton("ℹ️ HELP", callback_data="help"),
+                InlineKeyboardButton("💫 ABOUT", callback_data="abt")
+                ],[
+                InlineKeyboardButton("🍂 SUPPORT 🍂", url="https://t.me/PYRO_BOTZ_CHAT")
+                ]]
+                )
+            )
+    elif data == "help":
+        await query.message.edit_text(
+            text=HELP_MSG,
+            reply_markup=InlineKeyboardMarkup( [[
+                 InlineKeyboardButton("🔐 CLOSE", callback_data="close"),
+                 InlineKeyboardButton("↩️ BACK", callback_data="start")
+                 ]]
+                 )
+            )
 
+    elif data == "abt":
+        await query.message.edit_text(
+            text=ABOUT_TXT,
+            reply_markup=InlineKeyboardMarkup( [[
+                 InlineKeyboardButton("🔐 CLOSE", callback_data="close"),
+                 InlineKeyboardButton("↩️ BACK", callback_data="start")
+                 ]]
+                 )
+            )
+    elif data == "close":
+        await query.message.delete()
+        try:
+            await query.message.reply_to_message.delete()
+        except:
+            pass
 
-
-@Client.on_callback_query(filters.regex(r'^abt$'))   
-async def cb_abt(bot, cb):
-    await cb.message.edit_text("Talking is cheap, Read Code.",
-    reply_markup=InlineKeyboardMarkup(
-        [
-            [
-                InlineKeyboardButton("Source", url="https://github.com/subinps/Forward_2.0"),
-            ]
-        ]
-    )
-    )
