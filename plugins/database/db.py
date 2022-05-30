5f66c3ef240e56ca5d034f5c2d46b52b920a5a4c
KOSHI Dev <86915159+arjun-sangu@users.noreply.github.com>
2022-05-30 19:45:00 +0530

    Create db.py

---
+++ plugins/database/db.py
@@ -0,0 +1,18 @@
+import datetime
+import motor.motor_asyncio
+import Config
+
+class Database:
+    def __init__(self, uri, database_name):
+        self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
+        self.db = self._client[database_name]
+        self.col = self.db.users
+
+
+async def set_target(self, id, caption):
+        await self.col.update_one({'id': id}, {'$set': {'target': target}})
+
+
+async def get_target(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return user.get('target', None)
