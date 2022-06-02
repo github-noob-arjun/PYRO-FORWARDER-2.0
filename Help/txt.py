109b1d5d06fb01d7c9d4cbd26cccf3f68b855001
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-06-02 21:26:51 +0530

    Update txt.py

--- Help/txt.py
+++ Help/txt.py
@@ -1,7 +1,16 @@
 class pyro(object):
     START_MSG="""Hi {},👋\n\nThis is a simple bot to forward all messages from one channel to other🤩\n\n⚠️Warning\nYour account may get banned if you forward more files(from private channels). Use at Own Risk!!""" 
     HELP_MSG="""Available commands:-\n\n/index - To index a channel\n/forward - To start forwarding\n/total - Count total messages in DB\n/status - Check Current status\n/help - Help data\n/stop - To stop all running processes. \n/cleardb - clear all files in database.\n\nUse /index to index messages from a channel to database.\n\nAfter indexing you can start forwarding by using /forward.\n\n<b>Note:</b>\nYou will require the following data to index a channel:-\n\n<b>Channel Invite Link</b>:- If channel is a Private channel User needs to join channel to acces the messages. Please note that do not leave channel until forwarding completes.\n\n<b>Channel ID</b>:- If channel is a private channel you may need to enter Channel ID. Get it from @MKN_ID_Tgraph_bot.\n\n<b>SKIP_NO</b>:-From where you want to start Forwarding files.Give 0 if from starting\n\n<b>Caption</b>:- Custom Caption for forwarded files. Use 0 to use default captions."""      
-    ABOUT_TXT=""" #sOoN"""
+    ABOUT_TXT="""╭━━━━━━━━━━━━━━━➣
+┣👨‍💻 ᴅᴇᴠ 1 :  <a href=https://t.me/GitHub_noob>𝙂𝙞𝙩𝙃𝙪𝙗 𝙉𝙊𝙊𝘽</a>
+┣👨‍💻 ᴅᴇᴠ 2 : <a href=https://t.me/mr_MKN>𝙼𝚛.𝙼𝙺𝙽 𝚃𝙶</a>
+┣❣️ ᴄʀᴇᴅɪᴛs : <a href=https://github.com/subinps>SUBIN ❣️</a>
+┣🚀 sᴇʀᴠᴇʀ : <a href=https://dashboard.heroku.com>ʜᴇʀᴏᴋᴜ</a>
+┣💾 ᴅᴀᴛᴀʙᴀsᴇ : <a href=https://cloud.mongodb.com>ᴍᴏɴɢᴏ ᴅʙ</a>
+┣✏️ ʟᴀɴɢᴜᴀɢᴇ : <a href=https://www.python.org>ᴘʏᴛʜᴏɴ</a>
+┣📚 ʟɪʙʀʀʏ : <a href=https://github.com/pyrogram>ᴘʏʀᴏɢʀᴀᴍ</a>
+┣💥 sᴏᴜʀsᴇ ᴄᴏᴅᴇ : <a href=https://github.com/subinps/Forward_2.0>Forward_2.0</a>
+╰━━━━━━━━━━━━━━━➣  """
 
 
 
