1e118e2721e95ba126ab39732aa095ca977f6bfa
KOSHI Dev <86915159+arjun-sangu@users.noreply.github.com>
2022-06-02 10:55:14 +0530

    Update Index.py

--- plugins/Index.py
+++ plugins/Index.py
@@ -31,7 +31,7 @@ async def run(bot, message):
         try:
            # chat = await bot.ask(text = "To Index a channel you may send me the channel invite link, so that I can join channel and index the files.\n\nIt should be something like <code>https://t.me/xxxxxx</code> or <code>https://t.me/joinchat/xxxxxx</code>", chat_id = message.from_user.id, filters=filters.text, timeout=30)
            # channel=chat.text
-            chat = await bot.send_message(text = "To Index a channel you may send me the channel invite link, so that I can join channel and index the files.\n\nIt should be something like <code>https://t.me/xxxxxx</code> or <code>https://t.me/joinchat/xxxxxx</code>", chat_id = message.from_user.id, filters=filters.text, timeout=30)
+            chat = await bot.send_message(text = "To Index a channel you may send me the channel invite link, so that I can join channel and index the files.\n\nIt should be something like <code>https://t.me/xxxxxx</code> or <code>https://t.me/joinchat/xxxxxx</code>", chat_id = message.from_user.id, timeout=30)
             channel=chat.message.reply_to_message.text
             await chat.delete()
         except TimeoutError:
