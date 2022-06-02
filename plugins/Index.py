bd00b25b2fe60fa1f538380adb4d0d1310236b96
KOSHI Dev <86915159+arjun-sangu@users.noreply.github.com>
2022-06-02 16:08:22 +0530

    Update Index.py

--- plugins/Index.py
+++ plugins/Index.py
@@ -1,3 +1,9 @@
+# ====================================================================================================================================================================≠========================================================================================================================================================================================================================≠========================================================================================================================================================================================================================≠========================================================================================================================================================================================================================≠========================================================================================================================================================================================================================≠========================================================================================================================================================================================================================≠========================================================================================================================================================================================================================≠========================================================================================================================================================================================================================≠====================================================
+START_MSG="Hi {},\nThis is a simple bot to forward all messages from one channel to other\n\n⚠️Warning\nYour account may get banned if you forward more files(from private channels). Use at Own Risk!!"
+HELP_MSG="Available commands:-\n\n/index - To index a channel\n/forward - To start forwarding\n/total - Count total messages in DB\n/status - Check Current status\n/help - Help data\n/stop - To stop all running processes. \n/cleardb - clear all files in database.\n\nUse /index to index messages from a channel to database.\n\nAfter indexing you can start forwarding by using /forward.\n\n<b>Note:</b>\nYou will require the following data to index a channel:-\n\n<b>Channel Invite Link</b>:- If channel is a Private channel User needs to join channel to acces the messages. Please note that do not leave channel until forwarding completes.\n\n<b>Channel ID</b>:- If channel is a private channel you may need to enter Channel ID. Get it from @ChannelidHEXbot.\n\n<b>SKIP_NO</b>:-From where you want to start Forwarding files.Give 0 if from starting\n\n<b>Caption</b>:- Custom Caption for forwarded files. Use 0 to use default captions."
+ABOUT_TXT=""" #sOoN"""
+# ====================================================================================================================================================================≠========================================================================================================================================================================================================================≠========================================================================================================================================================================================================================≠========================================================================================================================================================================================================================≠========================================================================================================================================================================================================================≠========================================================================================================================================================================================================================≠========================================================================================================================================================================================================================≠========================================================================================================================================================================================================================≠====================================================
+
 import pytz
 from datetime import datetime
 from pyrogram import Client, filters
@@ -241,3 +247,41 @@ async def cb_handler(bot: Client, query: CallbackQuery):
         print(e)
         await m.reply_text(f"Error: {e}")
         pass
+
+    elif query.data == "start":
+        await query.message.edit_text(
+            text=START_MSG.format(query.from_user.first_name),
+            reply_markup=InlineKeyboardMarkup( [[
+                InlineKeyboardButton("ℹ️ HELP", callback_data="help"),
+                InlineKeyboardButton("💫 ABOUT", callback_data="abt")
+                ],[
+                InlineKeyboardButton("🍂 SUPPORT 🍂", url="https://t.me/PYRO_BOTZ_CHAT")
+                ]]
+                )
+            )
+    elif query.data == "help":
+        await query.message.edit_text(
+            text=HELP_MSG,
+            reply_markup=InlineKeyboardMarkup( [[
+                 InlineKeyboardButton("🔐 CLOSE", callback_data="close"),
+                 InlineKeyboardButton("↩️ BACK", callback_data="start")
+                 ]]
+                 )
+            )
+
+    elif query.data == "abt":
+        await query.message.edit_text(
+            text=ABOUT_TXT,
+            reply_markup=InlineKeyboardMarkup( [[
+                 InlineKeyboardButton("🔐 CLOSE", callback_data="close"),
+                 InlineKeyboardButton("↩️ BACK", callback_data="start")
+                 ]]
+                 )
+            )
+    elif query.data == "close":
+        await query.message.delete()
+        try:
+            await query.message.reply_to_message.delete()
+        except:
+            pass
+
