8f182fd3af369793086b53e65c45de3190c7fc1c
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2022-06-07 08:03:23 +0530

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -146,14 +146,9 @@ async def cb_handler(bot: Client, query: CallbackQuery):
     FROM=channel_id_
     try:
         async for MSG in bot.USER.search_messages(chat_id=FROM,offset=skip_no,limit=limit_no,filter=filter):
-            if channel_type == "public":
-                methord="bot"
-                channel=FROM
-                msg=await bot.get_messages(FROM, MSG.message_id)
-            elif channel_type == "private":
-                methord="user"
-                channel=str(FROM)
-                msg=await bot.USER.get_messages(FROM, MSG.message_id)
+            methord="user"
+            channel=str(FROM)
+            msg=await bot.USER.get_messages(FROM, MSG.message_id)
             msg_caption=""
             if caption is not None:
                 msg_caption=caption
